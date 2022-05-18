323e0e73894969bc772d75e3217f5c08f215461e
YasminHamed <98949809+YasminHamed@users.noreply.github.com>
2022-05-19 00:48:44 +0300

    php

---
+++ login_form.php
@@ -0,0 +1,124 @@
+<?php
+
+    @include 'config.php';
+    session_start();
+
+    $name = $email= $pass = $pass2 = $choice = "" ;
+    $nameErr = $emailErr = $passErr2 = $passErr = $choiceErr = "" ;
+
+    if(isset($_POST['submit'])) {
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $pass = md5($_POST['pass']);
+
+        $select = "SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
+
+        $result = mysqli_query($conn, $select);
+
+        if(mysqli_num_rows($result) > 0){
+
+            $row = mysqli_fetch_array($result);
+
+            if($row['choice'] == 'admin'){
+
+                $_SESSION['admin_name'] = $row['name'];
+                header("Location: admin_page.php");
+
+            }elseif($row['choice'] == 'user'){
+
+                $_SESSION['user_name'] = $row['name'];
+                header("Location: user_page.php");
+            }
+
+        }else{
+            $error[] = 'incorrect email or password!';
+        }
+
+    }
+
+?>
+<?php
+
+    // @include 'config.php';
+
+    // session_start();
+
+    
+    // $name = $email= $pass = $pass2 = $choice = "" ;
+    // $nameErr = $emailErr = $passErr2 = $passErr = $choiceErr = "" ;
+
+    // $email = $_POST["email"];
+    // $pass = $_POST["pass"];
+
+    // $check_email = mysqli_query($conn, "SELECT Email FROM crud where Email = '$email' ");
+    // if(mysqli_num_rows($check_email) > 0){
+    //     echo('Email Already exists');
+    // }
+    // else{
+    //     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //         $result = mysqli_query($conn, "INSERT INTO crud (Firstname, Lastname, Email, Password) VALUES ('$firstname', '$lastname', '$email', '$pass')");
+    //     }
+    //     echo('Record Entered Successfully');
+    // }
+
+
+    // if(isset($_POST['submit'])){
+
+    // $email = mysqli_real_escape_string($conn, $_POST['email']);
+    // $pass = md5($_POST['pass']);
+
+    // $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
+
+    // $result = mysqli_query($conn, $select);
+
+    // if(mysqli_num_rows($result) > 0){
+
+    //     $row = mysqli_fetch_array($result);
+
+    //     if($row['choice'] == 'admin'){
+
+    //         $_SESSION['admin_name'] = $row['name'];
+    //         header('location:admin_page.php');
+
+    //     }elseif($row['choice'] == 'user'){
+
+    //         $_SESSION['user_name'] = $row['name'];
+    //         header('location:user_page.php');
+
+    //     }
+        
+    // }else{
+    //     $error[] = 'incorrect email or password!';
+    // }
+
+    // };
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <form method="post" class="bg-white rounded-5 shadow-5-strong p-5">
+        <div class="title">Login</div>
+
+        <?php 
+            if(isset($error)) {
+                foreach($error as $error){
+                    echo '<span class="error2">'.$error.'</span>';
+                }
+            }
+        ?> 
+
+        <input name="email" type="email" placeholder="Email" class="user-input">
+
+        <input name="pass" type="password" placeholder="Password" class="user-input">
+        <p class="title3 ">don't have an account? <a href="register_form.php">register</a></p>
+        <input name="submit" type="submit" value="Start" class="submit-button">
+    </form>
+</body>
+</html>
---
+++ register_form.php
@@ -0,0 +1,115 @@
+<?php
+
+    @include 'config.php';
+
+    $name = $email= $pass = $pass2 = $choice = "" ;
+    $nameErr = $emailErr = $passErr2 = $passErr = $choiceErr = "" ;
+
+    if(isset($_POST['submit'])) {
+        $name = mysqli_real_escape_string($conn, $_POST['name']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $pass = md5($_POST['pass']);
+        $pass2 = md5($_POST['pass2']);
+        $choice = $_POST['choice'];
+
+        $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
+
+        $result = mysqli_query($conn, $select);
+
+        if(mysqli_num_rows($result) > 0){
+            $error[] = 'user already exist!';
+        } else {
+            if (empty($_POST["name"])) {
+                $nameErr = "Name is required!";
+            } else {
+                $name = test_input($_POST["name"]);
+                if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+                    $nameErr = "Only letters and white spaces are allowed";
+                }
+            }
+
+            if (empty($_POST["email"])) {
+                $emailErr = "Email is required!";
+            } else {
+                $email = test_input($_POST["email"]);
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $emailErr = "Invalid email format!";
+                }
+            }
+
+            if (empty($_POST["choice"])) {
+                $choiceErr = "choice is choice!";
+            }
+
+            if (empty($_POST["pass"])) {
+                $passErr = "Password is required!";
+            } else {
+                $pass = test_input($_POST["pass"]);
+                if (!preg_match("/^S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/",$pass )) {
+                    $passErr = "Invalid password!  (must contain at least 8 characters/one lowercase letter/one uppercase letter / a number)";
+                }else{
+                    $insert = "INSERT INTO user_form(name, email, password, user_type) VALUES('$name', '$email', '$pass', '$choice')";
+                    mysqli_query($conn, $insert);
+                    header('location:login_form.php');
+                }
+            }
+
+            if ($_POST["pass2"] !== $_POST["pass"]) {
+                $passErr2 = "Password dismatch!";
+            }
+        }
+
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php
+        function test_input($data){
+            $data = trim($data); //ignore the spaces
+            $data = stripslashes($data); //ignore slashes
+            $data = htmlspecialchars($data);
+            return $data;
+        };
+
+    ?>
+    <form method="post" class="bg-white  rounded-5 shadow-5-strong p-5">
+        <div class="title2">Register </div>
+        <?php 
+            if(isset($error)) {
+                foreach($error as $error){
+                    echo '<span class="error2">'.$error.'</span>';
+                }
+            }
+        ?>         <p class="error"><?php echo $nameErr;  ?></p>
+        <input type="text" placeholder="UserName" name="name" class="user-input">
+
+        <span class="error"><?php echo $emailErr;  ?></span>
+        <input type="email" placeholder="Email" name="email" class="user-input">
+
+        <span class="error"><?php echo $passErr;  ?></span>
+        <input type="password" placeholder="Password" name="pass" class="user-input">
+
+        <span class="error"><?php echo $passErr2;  ?></span>
+        <input type="password" placeholder="Confirm Password" name="pass2" class="user-input">
+
+        <span class="error"><?php echo $choiceErr;  ?></span>
+        <select class="user-input" name="choice">
+            <option value="admin">admin</option>
+            <option value="user">user</option>
+        </select>
+
+        <p class="title3 ">already have an account? <a href="login_form.php">Login</a></p>
+        <input type="submit" name="submit" value="Create account" class="submit-button">
+    </form>
+</body>
+</html>
