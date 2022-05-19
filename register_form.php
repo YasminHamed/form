3f084cdc2c5b3e32a4db9f9b2ceca0500bc0a150
YasminHamed <98949809+YasminHamed@users.noreply.github.com>
2022-05-20 00:29:38 +0300

    Update register_form.php

--- register_form.php
+++ register_form.php
@@ -1,76 +1,51 @@
 <?php
 
-    @include 'config.php';
+@include 'config.php';
 
-    $name = $email= $pass = $pass2 = $choice = "" ;
-    $nameErr = $emailErr = $passErr2 = $passErr = $choiceErr = "" ;
+if(isset($_POST['submit'])){
 
-    if(isset($_POST['submit'])) {
-        $name = mysqli_real_escape_string($conn, $_POST['name']);
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $pass = md5($_POST['pass']);
-        $pass2 = md5($_POST['pass2']);
-        $choice = $_POST['choice'];
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = md5($_POST['password']);
+   $cpass = md5($_POST['cpassword']);
+   $user_type = $_POST['user_type'];
 
-        $select = " SELECT * FROM user_form WHERE email = '$email' ";
+   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
 
-        $result = mysqli_query($conn, $select);
+   $result = mysqli_query($conn, $select);
 
-        if(mysqli_num_rows($result) > 0){
-            $error[] = 'email already exist!';
-        } else {
-            if (empty($_POST["name"])) {
-                $nameErr = "Name is required!";
-            } else {
-                $name = test_input($_POST["name"]);
-                if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-                    $nameErr = "Only letters and white spaces are allowed";
-                }
-            }
+   if(mysqli_num_rows($result) > 0){
 
-            if (empty($_POST["email"])) {
-                $emailErr = "Email is required!";
-            } else {
-                $email = test_input($_POST["email"]);
-                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    $emailErr = "Invalid email format!";
-                }
-            }
+      $error[] = 'user already exist!';
 
-            if (empty($_POST["choice"])) {
-                $choiceErr = "choice is choice!";
-            }
+   }else{
 
-            if (empty($_POST["pass"])) {
-                $passErr = "Password is required!";
-            } else {
-                $pass = test_input($_POST["pass"]);
-                if (!preg_match("/^S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/",$pass )) {
-                    $passErr = "Invalid password!  (must contain at least 8 characters/one lowercase letter/one uppercase letter / a number)";
-                }else{
-                    $insert = "INSERT INTO user_form(name, email, password, user_type) VALUES('$name', '$email', '$pass', '$choice')";
-                    mysqli_query($conn, $insert);
-                    header('location:login_form.php');
-                }
-            }
+      if($pass != $cpass){
+         $error[] = 'password not matched!';
+      }else{
+          
+         $insert = "INSERT INTO user_form(name, email, password, user_type) VALUES('$name','$email','$pass','$user_type')";
+         mysqli_query($conn, $insert);
+         header('location:login_form.php');
+      }
+   }
 
-            if ($_POST["pass2"] !== $_POST["pass"]) {
-                $passErr2 = "Password dismatch!";
-            }
-        }
+};
 
-    }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>register form</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
     <?php
@@ -84,32 +59,30 @@
     ?>
     <form method="post" class="bg-white  rounded-5 shadow-5-strong p-5">
         <div class="title2">Register </div>
-        <?php 
-            if(isset($error)) {
+        <?php
+            if(isset($error)){
                 foreach($error as $error){
                     echo '<span class="error2">'.$error.'</span>';
-                }
-            }
-        ?>         <p class="error"><?php echo $nameErr;  ?></p>
+                };
+            };
+        ?>
         <input type="text" placeholder="UserName" name="name" class="user-input">
 
-        <span class="error"><?php echo $emailErr;  ?></span>
-        <input type="email" placeholder="Email" name="email" class="user-input">
+        <input type="text" placeholder="Email" name="email" class="user-input">
 
-        <span class="error"><?php echo $passErr;  ?></span>
-        <input type="password" placeholder="Password" name="pass" class="user-input">
+        <input type="password" placeholder="Password" name="password" class="user-input">
 
-        <span class="error"><?php echo $passErr2;  ?></span>
-        <input type="password" placeholder="Confirm Password" name="pass2" class="user-input">
+        <input type="password" placeholder="Confirm Password" name="cpassword" class="user-input">
 
-        <span class="error"><?php echo $choiceErr;  ?></span>
-        <select class="user-input" name="choice">
-            <option value="admin">admin</option>
+        <select class="user-input" name="user_type">
             <option value="user">user</option>
+            <option value="admin">admin</option>
         </select>
 
         <p class="title3 ">already have an account? <a href="login_form.php">Login</a></p>
         <input type="submit" name="submit" value="Create account" class="submit-button">
     </form>
+
+
 </body>
 </html>
